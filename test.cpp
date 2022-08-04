bff7954ada4de9c71c33a513635803314afd0558
B2BM2M <94427947+B2BM2M@users.noreply.github.com>
2022-08-03 22:45:11 -0700

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,78 +1,23 @@
 #include <iostream>
-#include <string.h>
-#include <map>
-using namespace std;
+#include <bits/stdc++.h>
 
-void count_string (string &tmpstring, int &most_vowel, int &most_consonants, int &accent_character);
+using namespace std;
 
 int main()
 {
-/*     int mymost_consonants = 0;
-    int mymost_vowel = 0;
-    int myaccent_character = 0; */
-    map<int, string> WordResult = {};
+    cout<<"Hello World"<<endl;
+    unordered_map<string, vector<string>> edges;
+    unordered_map<string, bool> hasParent;
+    edges["mammal"].push_back("feline");
+   // hasParent["a"] = TRUE;
+    edges["mammal"].push_back("canine");
+    edges["mammal"].push_back("monotreme");
+    //edges["dog"].push_back("d");
+    edges["feline"].push_back("jaguar");
+    edges["feline"].push_back("lion");
+    edges["feline"].push_back("cat");
 
-    string array_of_string [] = {"strengthsaeiueouy", "ants 1", "turkey", "facetious"};
-    int getArrayLength = sizeof(array_of_string)/sizeof(string);
-
-    // cout << getArrayLength;
-
-    for (int i = 0; i < getArrayLength; i++)
-    //for (int i = 0; i < 4; i++)
-    {
-        int  mymost_vowel = 0;
-        int  mymost_consonants = 0;
-        int myaccent_character = 0;
-
-        string getArrayWord = array_of_string[i];
-    //    cout << getArrayWord << endl;
-        count_string(getArrayWord, mymost_vowel, mymost_consonants, myaccent_character);
-        
-        if (mymost_vowel >= mymost_consonants && mymost_vowel >= myaccent_character)
-        {
-            WordResult.insert({mymost_vowel, getArrayWord});
-            cout << getArrayWord << " Has more Vowel " << '\t' << i << endl;
-            
-        }
-        else if (mymost_consonants >= mymost_vowel && mymost_consonants > myaccent_character)
-        {
-            WordResult.insert({mymost_consonants, getArrayWord});
-            cout << getArrayWord << " Has more Consonants " << '\t' << i << endl;
-           
-        }
-        else 
-        {
-            WordResult.insert({myaccent_character, getArrayWord});
-            cout << getArrayWord << " Accent  characters" << '\t' << i << endl;
-           
-        }
-
-    }
-    
-    for (auto itr = WordResult.begin(); itr !=WordResult.end(); itr++)
-    {
-        cout << itr->first << '\t' << itr->second <<'\n';
-    } 
+    cout << edges["feline"][0] << ", " << edges["feline"][1] << ", "<< edges["feline"][2] << ", ";
+  
 
 }
-
-void count_string (string &inputWords, int &most_vowel, int &most_consonants, int &accent_character)
-{
-    for (int i = 0; i <inputWords.length(); i++)
-    {
-        if (inputWords.at(i) == 'a' || inputWords.at(i) == 'A' || inputWords.at(i) =='e' || inputWords.at(i) =='E' || inputWords.at(i) =='i' || inputWords.at(i) =='I' || inputWords.at(i) =='o' ||
-            inputWords.at(i) =='O' || inputWords.at(i) =='u' || inputWords.at(i) =='U' || inputWords.at(i) =='y' || inputWords.at(i) =='Y' )
-        {
-            most_vowel++; 
-        }
-        else if ((inputWords.at(i)>='a' && inputWords.at(i)<= 'z') || (inputWords.at(i) >= 'A' && inputWords.at(i)<= 'Z')) 
-        {
-            most_consonants++;
-        }
-        else 
-        { 
-            accent_character++;
-        }
-    }
-}
-
